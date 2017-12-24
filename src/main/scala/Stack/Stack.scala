127aaf0b90903f40d7e808becc482bafe97a313b
jsburke <jsburke@bu.edu>
2017-12-23 22:35:18 -0500

    rough out innards a little

--- src/main/scala/Stack/Stack.scala
+++ src/main/scala/Stack/Stack.scala
@@ -1,38 +1,83 @@
 
-package FIFO
+package Stack
 
 import chisel3._
 
 /////////////////////////////////////////////////////////////////
 //
-// FIFO : basic FIFO of parametrizable depth
+// Stack : basic Stack of parametrizable depth
+//         first value in is last out
 //
-//        introduces some simple polymorphism
+//         introduces some simple polymorphism
 //
 /////////////////////////////////////////////////////////////////
 
-class FIFOIn[T <: Data](gen: T) extends Bundle{
+class StackIn[T <: Data](gen: T) extends Bundle{
   val read_enable  = Input(Bool)
   val write_enable = Input(Bool)
   val write_data   = Input(gen)
   val flush        = Input(Bool)
 }
 
-class FIFOOut[T <: Data](gen: T) extends Bundle{
+class StackOut[T <: Data](gen: T) extends Bundle{
   val read_data = Output(gen)
   val full      = Output(Bool)
   val empty     = Output(Bool)
 }
 
-class FIFO[T <: Data](gen: T, val depth: Int) extends Module{
+class Stack[T <: Data](gen: T, val depth: Int) extends Module{
   val io = IO(new Bundle{
-    val in  = FIFOIn(gen)
-    val out = FIFOOut(gen)
+    val in  = StackIn(gen)
+    val out = StackOut(gen)
   })
 
-  val fifo_mem   = Mem(depth, gen)
-  val p_write    = Reg(init = UInt(0, depth))
-  val p_read     = Reg(init = UInt(0, depth))
-  val fifo_empty = Reg(init = Bool(true))
+  val stack_mem   = Mem(depth, gen)
+  val p_stack     = Reg(init = UInt(0, depth)) // should make own class for inc, dec, max, etc other methods for ease
+  val stack_empty = Reg(init = Bool(true))
+  val stack_full  = Reg(init = Bool(false))
+  val data_out    = Reg(gen)
 
+  when(io.in.flush) {
+    p_stack     := 0.U
+    stack_empty := true.B
+    data_out    := stack_mem(0.U)
+  }
+
+  when(stack_empty) {
+    when(io.in.read_enable && io.in.write_enable) {
+      data_out := io.in.write_data // pass through on empty
+    }
+    .elsewhen(io.in.read_enable) {
+      data_out := stack_mem(0.U) // give a dummy value
+    }
+    .elsewhen(io.in.write_enable) {
+      data_out := stack_mem(0.U)
+      stack_empty := false.B
+      stack_mem(p_stack) := io.in.write_data
+      p_stack     := p_stack + 1.U
+    }
+  }
+  .otherwise { // not empty
+    when(io.in.read_enable && io.in.write_data) {
+      data_out := io.in.write_data  // pass through, not DRY
+    }
+    .elsewhen(io.in.read_enable) {
+      data_out := stack_mem(p_stack)
+      stack_empty := (p_stack - 1.U) === 0.U
+      p_stack     := p_stack - 1.U
+      stack_full  := false.B  // can guarantee
+    }
+    .elsewhen(io.in.write_enable) {
+      stack_full         := (p_stack + 1.U) === UInt(depth)
+      stack_mem(p_stack) := io.in.write_data
+      p_stack            := p_stack + 1.U
+    }
+  }
+  
+  io.out.full      := stack_full
+  io.out.empty     := stack_empty
+  io.out.read_data := data_out
 }
+
+
+
