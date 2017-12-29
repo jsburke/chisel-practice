f19b969ce89ed30a45e8b60a0753f7989e55315d
jsburke <jsburke@bu.edu>
2017-12-29 16:35:58 -0500

    remove unused traits

--- src/main/scala/Float/FloatTypes.scala
+++ src/main/scala/Float/FloatTypes.scala
@@ -2,7 +2,6 @@
 package float
 
 import chisel3._
-import chisel3.util.Cat
 
 //////////////////////////////////////////////////////
 //
@@ -10,35 +9,6 @@ import chisel3.util.Cat
 //         floating point values
 //
 //////////////////////////////////////////////////////
-
-// Not-a-Number
-
-trait HasNaN {
-  val NaN    = Bool()
-}
-
-// options for zero
-
-trait HasSplitZero {
-  val posZero = Bool()
-  val negZero = Bool()
-}
-
-trait HasUnifiedZero {
-  val zero    = Bool()
-}
-
-// options to extend floating point definitions
-
-trait FloatOptions {
-  val subNorm = Bool()
-
-  val posInf  = Bool()
-  val negInf  = Bool()
-}
-
-trait HasSimpleOptions  extends FloatOptions with HasNaN with HasUnifiedZero
-trait HasFancyOptions   extends FloatOptions with HasNaN   with HasSplitZero
 
 class floatBase(exp_width: Int, mant_width: Int) extends Bundle {
   val sign     = Bool()
@@ -50,15 +20,17 @@ class floatBase(exp_width: Int, mant_width: Int) extends Bundle {
 
 class floatExtended(exp_width: Int, mant_width: Int) extends floatBase(exp_width, mant_width) {
   // deal with zeros
-  def zero:    Bool = ~(exponent.orR | mantissa.orR)
-  def posZero: Bool = ~(sign | zero)
-  def negZero: Bool = ~(~sign | zero)
+  val zero    = ~(exponent.orR | mantissa.orR)
+  val posZero = ~(sign | zero)
+  val negZero = ~(~sign | zero)
 
   // deal with infinities
-  def inf: Bool     = exponent.andR & ~(mantissa.orR)
-  def posInf: Bool  = ~sign & inf
-  def negInf: Bool  = sign & inf
+  val inf     = exponent.andR & ~(mantissa.orR)
+  val posInf  = ~sign & inf
+  val negInf  = sign & inf
  
-  def NaN: Bool     = exponent.andR & mantissa.orR
-  def denorm: Bool  = ~(exponent.andR) & mantissa.orR
+  val NaN     = exponent.andR & mantissa.orR
+  val denorm  = ~(exponent.andR) & mantissa.orR
+
+  override def cloneType = new floatExtended(exp_width, mant_width).asInstanceOf[this.type]
 }
