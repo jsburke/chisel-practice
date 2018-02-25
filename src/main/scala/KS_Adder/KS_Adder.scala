7aeaced2e3482ef4f019d785ed440fa98afe7cf4
jsburke <jsburke@bu.edu>
2018-02-25 14:36:54 -0500

    complete methods for generating carry signals

--- src/main/scala/KS_Adder/KS_Adder.scala
+++ src/main/scala/KS_Adder/KS_Adder.scala
@@ -15,13 +15,28 @@ class KS_Adder (val width: Int, val useCarry: Boolean) extends Module {
 
   val carry_0 = io.carryIn.getOrElse(0.U)  // carry lsb is either input or hardwire zero
 
+
+  // simple shortenings in KS
   def propagate(i: Int): UInt = io.a(i) ^ io.b(i)
   def generate(i: Int):  UInt = io.a(i) & io.b(i)
 
-  def genMids(i: Int): UInt = 1.U // NB clearly dummy for now
+  // genMids derives the second to second-to-last terms in carries where i > 1
+  def genMids(i: Int): Seq[UInt] = {
+    require(i > 0, "genMids cannot be invoked with i <= 0")
+
+    def midGuts(i: Int, prev: Option[UInt]): Seq[UInt] = {
+      if (i == 1) Seq()  // base case gives empty Seq, works for all i, even C_1 which has no Mids
+      else {
+        val newPrev = prev.getOrElse(1.U) & propagate(i - 1)
+        Seq(newPrev & generate(i - 2)) ++ midGuts(i - 1, Some(newPrev))
+      }
+    }
+
+  midGuts(i, None)
+  }
 
   def genTail(i: Int): UInt = {
-    require(i > 0, "genTail invoked with i <= 0")
+    require(i > 0, "genTail cannot be invoked with i <= 0")
     if(i == 1) propagate(0) & carry_0 // base case
     else propagate(i - 1) & genTail(i - 1)
   }
@@ -29,6 +44,8 @@ class KS_Adder (val width: Int, val useCarry: Boolean) extends Module {
   def carry(i: Int): UInt = {
     if (i == 0) carry_0
     else if (i == 1) generate(0) | (propagate(0) & carry_0)
-    else Seq(generate(i - 1), genMids(i), genTail(i)) reduceLeft(_|_)
+    else Seq(generate(i - 1)) ++ genMids(i) ++ Seq(genTail(i)) reduceLeft(_|_)
   }
+
+  
 }
