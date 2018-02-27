3cd989752e91e1a0246ea48e0194d82222060801
jsburke <jsburke@bu.edu>
2018-02-27 12:59:05 -0500

    suspect false positives.  test further

--- src/test/scala/KS_Adder/KS_AdderUnitTest.scala
+++ src/test/scala/KS_Adder/KS_AdderUnitTest.scala
@@ -4,16 +4,21 @@ package KS_Adder
 import chisel3._
 import chisel3.iotesters.{ChiselFlatSpec, Driver, PeekPokeTester}
 
+//Stream.continually(nextInt(100)).take(10)
+
 class KS_AdderUnitTest(c: KS_Adder) extends PeekPokeTester(c) {
 
-  for (i <- 0 until 6){
-    val rand_in1 = rnd.nextInt(c.width)
-    val rand_in2 = rnd.nextInt(c.width)
+  val base = 1 to 4 
+  val in_a = base map (_*2)
+  val in_b = base map (_ << 2 + 1)
 
-    poke(c.io.a, rand_in1)
-    poke(c.io.b, rand_in2)
+  for (i <- 0 until 4){
+    val a_sel = in_a(i)
+    val b_sel = in_b(i) 
+    poke(c.io.a, a_sel)
+    poke(c.io.b, b_sel)
     step(1)
-    val rand_sum = rand_in1 + rand_in2 
+    val rand_sum = a_sel + b_sel 
     val mask     = BigInt("1"*c.width, 2) 
     expect(c.io.sum, rand_sum & mask)
 //    expect(c.io.cout, rand_sum % 1)
